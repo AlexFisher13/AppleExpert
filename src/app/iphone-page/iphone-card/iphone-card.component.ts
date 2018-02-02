311e8ca427d53b13ebf4cfbb73a948983c4d0ba8
Tkachenko AI <alexfisher13@yandex.ru>
2018-02-02 10:04:50 +0300

    add main pages, add iphone-cards

--- src/app/iphone-page/iphone-card/iphone-card.component.ts
+++ src/app/iphone-page/iphone-card/iphone-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'iphone-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iphone-card.component.css']
 })
 export class IphoneCardComponent implements OnInit {
+
+  @Input() iphone;
 
   constructor() { }
 
--- src/app/iphone-page/iphone-page.component.ts
+++ src/app/iphone-page/iphone-page.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IphonePageComponent implements OnInit {
 
+  iphones = [
+    {name: 'iPhone', description: '2007', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 3g', description: '2008', url:'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 3gs', description: '2009', url:'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 4', description: '2010', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 4s', description: '2011', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 5', description: '2012', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 5c', description: '2013', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 5s', description: '2013', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 6', description: '2014', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 6 Plus', description: '2014', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 6s', description: '2015', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 6s Plus', description: '2015', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone SE', description: '2016', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 7', description: '2016', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 7 Plus', description: '2016', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 8', description: '2017', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone 8 Plus', description: '2017', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+    {name: 'iPhone X', description: '2017', url: 'https://mobidevices.ru/images/2013/05/%D0%9F%D0%B5%D1%80%D0%B2%D1%8B%D0%B9-iPhone.jpeg'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
